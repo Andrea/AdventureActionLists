e4e641f7f2a8a528a5a2f716b9d7c4cfdd06ce4a
Andrea <roundcrisis@gmail.com>
2013-07-24 11:52:17 +0100

    Rough version of action list without parallel working

---
+++ ActionList/Action.cs
@@ -0,0 +1,18 @@
+﻿namespace ActionList
+{
+	public interface IAction
+	{
+		 bool IsFinished { get; set; }
+
+		void Update();
+	}
+
+	public class WalkTo : IAction
+	{
+		public bool IsFinished { get; set; }
+		public void Update()
+		{
+				IsFinished = true;
+		}
+	}
+}
---
+++ ActionList/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ActionList
+{
+	public class ActionList
+	{
+		private SortedList<int, IAction> _orderedActions;
+
+		public ActionList()
+		{
+			_orderedActions = new SortedList<int, IAction>();
+		}
+
+		public int ActionCount
+		{
+			get { return _orderedActions.Count(); }
+		}
+		public void AddActions(IAction action)
+		{
+			var len = !_orderedActions.Any() ? 0 : _orderedActions.Count();
+			if (action != null)
+				_orderedActions.Add(len+1, action);
+		}
+
+		public void Update()
+		{
+			if (!_orderedActions.Any())
+				return;
+
+			_orderedActions.First().Value.Update();
+			_orderedActions.First().Value.IsFinished = true;
+			_orderedActions.Remove(1);
+
+		}
+	}
+	[TestFixture]
+	public class ActionListTests
+	{
+		private ActionList _actionList;
+
+		[Test]
+		public void When_update_Then_one_less_action()
+		{
+			_actionList = new ActionList();
+			_actionList.AddActions(new WalkTo());
+			_actionList.AddActions(new WalkTo());
+			_actionList.Update();
+			Assert.AreEqual(1, _actionList.ActionCount);
+		}
+	}
+}
