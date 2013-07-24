894a88ed80c2c7a9d72492276e4de49a0667960c
Andrea <roundcrisis@gmail.com>
2013-07-24 12:08:28 +0100

    actually this commmit would be a rough working version

---
+++ ActionList/ActionList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ActionList
+{
+	public class ActionList
+	{
+		private readonly SortedList<int, IAction> _orderedActions;
+
+		public ActionList()
+		{
+			_orderedActions = new SortedList<int, IAction>();
+		}
+
+		public int ActionCount
+		{
+			get { return _orderedActions.Count(); }
+		}
+		public void AddActions(IAction action)
+		{
+			var len = !_orderedActions.Any() ? 0 : _orderedActions.Count();
+			if (action != null)
+				_orderedActions.Add(len+1, action);
+		}
+
+		public void Update()
+		{
+			if (!_orderedActions.Any())
+				return;
+
+			_orderedActions.First().Value.Update();
+			_orderedActions.First().Value.IsFinished = true;
+			_orderedActions.Remove(1);
+
+		}
+	}
+}
--- ActionList/Class1.cs
+++ ActionList/Class1.cs
@@ -1,40 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
 namespace ActionList
 {
-	public class ActionList
-	{
-		private SortedList<int, IAction> _orderedActions;
-
-		public ActionList()
-		{
-			_orderedActions = new SortedList<int, IAction>();
-		}
-
-		public int ActionCount
-		{
-			get { return _orderedActions.Count(); }
-		}
-		public void AddActions(IAction action)
-		{
-			var len = !_orderedActions.Any() ? 0 : _orderedActions.Count();
-			if (action != null)
-				_orderedActions.Add(len+1, action);
-		}
-
-		public void Update()
-		{
-			if (!_orderedActions.Any())
-				return;
-
-			_orderedActions.First().Value.Update();
-			_orderedActions.First().Value.IsFinished = true;
-			_orderedActions.Remove(1);
-
-		}
-	}
 	[TestFixture]
 	public class ActionListTests
 	{
